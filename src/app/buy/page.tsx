341c09cb6bdd74748a8930050550a3995d62a22f
gabriel <gabrielnextsc@gmail.com>
2025-10-05 17:42:38 +0000

    Assim que eu clicar no botão eu já quero que o vídeo comece instantaneam

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -9,7 +9,7 @@ export default function BuyPage() {
         <div className="relative" style={{ paddingBottom: '56.25%', height: 0 }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://drive.google.com/file/d/1egMvBOIyQBfC0fm8FaZ66vPbi9nbMHaf/preview"
+            src="https://drive.google.com/file/d/1egMvBOIyQBfC0fm8FaZ66vPbi9nbMHaf/preview?autoplay=1"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
