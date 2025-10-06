5631d6510a1234d50fd9105e3919d9591f66d7f7
gabriel <gabrielnextsc@gmail.com>
2025-10-06 05:06:06 +0000

    acho que se botar autoplay no video some aquela caixinha ali

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -174,19 +174,11 @@ export default function LandingPage() {
                        <div className="aspect-video w-full overflow-hidden relative rounded-lg">
                           <iframe
                             className="absolute top-0 left-0 w-full h-full"
-                            src="https://drive.google.com/file/d/1ifdXxWLrc7A7_ccUbyrCCitNg_Ctqb3l/preview"
+                            src="https://drive.google.com/file/d/1ifdXxWLrc7A7_ccUbyrCCitNg_Ctqb3l/preview?autoplay=1&mute=1"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                             title="VENENO INVISÍVEL"
                           ></iframe>
-                          <div className="absolute top-2 right-2 w-[108px] h-[29px] z-10 pointer-events-none">
-                            <Image
-                              src="https://barra.sistema.gov.br/v1/assets/govbr.webp"
-                              layout="fill"
-                              objectFit="contain"
-                              alt="Gov.br Logo"
-                            />
-                          </div>
                         </div>
                       <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl">
                         VENENO INVISÍVEL: Saiba Onde Fazer o Teste Gratuito e Conheça o 'Canudo de Defesa' Que Você Precisa Ter JÁ!
