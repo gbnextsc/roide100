2727a4dbfb3bf28bc43e5bd7a61a59d1ca334897
gbnextsc <gabrielnextsc@gmail.com>
2025-10-06 13:59:00 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,6 @@ export const metadata: Metadata = {
   metadataBase: new URL('http://localhost:9002'),
   title: 'Campanha Oficial de Prevenção ao Metanol',
   description: 'Receba seu canudo detector de metanol em casa, pagando apenas o frete. Uma campanha do Governo Federal para proteger sua saúde.',
-  icons: {
-    icon: 'https://www.gov.br/++theme++padrao_govbr/img/favicon.ico',
-  }
 };
 
 export default function RootLayout({
@@ -23,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://www.gov.br/++theme++padrao_govbr/img/favicon.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
